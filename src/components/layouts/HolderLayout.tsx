77d0e00a9b3885cddd58b8b7d62e293e301a484d
MUHAMMAD-Tech <64703589+MUHAMMAD-Tech@users.noreply.github.com>
2026-01-07 22:38:52 +0600

    Update HolderLayout.tsx

--- src/components/layouts/HolderLayout.tsx
+++ src/components/layouts/HolderLayout.tsx
@@ -134,7 +134,7 @@ export function HolderLayout() {
         <div className="flex flex-col h-full">
           {/* Logo */}
           <div className="p-6 border-b border-border">
-<img  src="https://raw.githubusercontent.com/MUHAMMAD-Tech/Res-/refs/heads/MUHAMMAD-Tech/let/img/resLogoG.svg" />
+<img  src="https://raw.githubusercontent.com/MUHAMMAD-Tech/Res-/refs/heads/MUHAMMAD-Tech/let/img/resLogoG.svg" className="logh" />
             <h1 className="text-2xl font-bold gradient-text">Lethex</h1>
             <p className="text-xs text-muted-foreground mt-1">Fund Dashboard</p>
           </div>
@@ -173,7 +173,7 @@ export function HolderLayout() {
 
         {/* Mobile Header */}
         <header className="lg:hidden flex items-center justify-between p-4 border-b border-border bg-card">
-<img  src="https://raw.githubusercontent.com/MUHAMMAD-Tech/Res-/refs/heads/MUHAMMAD-Tech/let/img/resLogoG.svg" />
+<img  src="https://raw.githubusercontent.com/MUHAMMAD-Tech/Res-/refs/heads/MUHAMMAD-Tech/let/img/resLogoG.svg" className="logh" />
           <h1 className="text-xl font-bold gradient-text">Lethex</h1>
           <div className="flex items-center gap-2">
             <Header />
@@ -187,7 +187,7 @@ export function HolderLayout() {
               <div className="flex flex-col h-full">
                 {/* Logo */}
                 <div className="p-6 border-b border-border">
-<img  src="https://raw.githubusercontent.com/MUHAMMAD-Tech/Res-/refs/heads/MUHAMMAD-Tech/let/img/resLogoG.svg" />
+<img  src="https://raw.githubusercontent.com/MUHAMMAD-Tech/Res-/refs/heads/MUHAMMAD-Tech/let/img/resLogoG.svg" className="logh"/>
                   <h1 className="text-2xl font-bold gradient-text">Lethex</h1>
                   <p className="text-xs text-muted-foreground mt-1">Fund Dashboard</p>
                 </div>
