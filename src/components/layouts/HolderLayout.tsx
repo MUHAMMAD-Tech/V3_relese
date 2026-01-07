1f93c7c8375c4ef7ddbe3d17e85caa4414274c96
MUHAMMAD-Tech <64703589+MUHAMMAD-Tech@users.noreply.github.com>
2026-01-08 00:23:32 +0600

    Update HolderLayout.tsx

--- src/components/layouts/HolderLayout.tsx
+++ src/components/layouts/HolderLayout.tsx
@@ -174,6 +174,7 @@ export function HolderLayout() {
         {/* Mobile Header */}
         <header className="lg:hidden flex items-center justify-between p-4 border-b border-border bg-card">
 <div className="flass">
+<img  src="https://raw.githubusercontent.com/MUHAMMAD-Tech/Res-/refs/heads/MUHAMMAD-Tech/let/img/resLogoG.svg" className="logh" />
           <h1 className="hww text-xl font-bold gradient-text">ethex</h1>
 </div>
           <div className="flex items-center gap-2">
@@ -188,6 +189,7 @@ export function HolderLayout() {
               <div className="flex flex-col h-full">
                 {/* Logo */}
                 <div className="p-6 border-b border-border">
+
                   <h1 className="text-2xl font-bold gradient-text">Lethex</h1>
                   <p className="text-xs text-muted-foreground mt-1">Fund Dashboard</p>
                 </div>
