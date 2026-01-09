80a618965b1c58fcf239bfc311c2a2f631fd7126
MUHAMMAD-Tech <64703589+MUHAMMAD-Tech@users.noreply.github.com>
2026-01-09 22:52:32 +0600

    Update AdminLayout.tsx

--- src/components/layouts/AdminLayout.tsx
+++ src/components/layouts/AdminLayout.tsx
@@ -121,7 +121,10 @@ export function AdminLayout() {
 
         {/* Mobile Header */}
         <header className="lg:hidden flex items-center justify-between p-4 border-b border-border bg-card">
-          <h1 className="text-xl font-bold gradient-text">LETHEX</h1>
+          <div className="flass">
+<img  src="https://raw.githubusercontent.com/MUHAMMAD-Tech/Res-/refs/heads/MUHAMMAD-Tech/let/img/resLogoG.svg" className="logh" />
+          <h1 className="hww text-xl font-bold gradient-text">ethex</h1>
+</div>
           <div className="flex items-center gap-2">
             <Header />
             <Sheet open={mobileMenuOpen} onOpenChange={setMobileMenuOpen}>
