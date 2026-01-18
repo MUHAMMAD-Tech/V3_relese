88d71dcedd555704eef20214f6588e0895c1bd56
MUHAMMAD-Tech <64703589+MUHAMMAD-Tech@users.noreply.github.com>
2026-01-18 13:22:34 +0600

    Update HolderTransactionsPage.tsx

--- src/pages/holder/HolderTransactionsPage.tsx
+++ src/pages/holder/HolderTransactionsPage.tsx
@@ -452,8 +452,8 @@ export default function HolderTransactionsPage() {
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
-              width="60"
-              height="60"
+              width="120"
+              height="120"
               viewBox="0 0 24 24"
               fill="none"
               className="mr-1"
